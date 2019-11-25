e15c4e9cbee550468a098191e574d41a70172a5e
BLANE THOMPSON <20160034@student.centralbeds.ac.uk>
2019-11-25 15:51:33 +0000

    Merged objects

--- calling-all-haters/app.py
+++ calling-all-haters/app.py
@@ -1,7 +1,58 @@
+import random
+import string
+
+from compress import compress_response
 from quart import Quart, jsonify, render_template, request, session, websocket
 
-import objects
-from compress import compress_response
+
+def generateHex(length=8):
+    return "".join(random.choices(list(string.hexdigits), k=8))
+
+
+class Game:
+    __slots__ = ['players', 'settings', 'decks']
+
+    def __init__(self, data, host):
+        self.settings = data
+        self.players = []
+        self.decks = []
+
+class Player:
+    __slots__ = ['name', 'display_name', 'id', 'points', 'deck', 'is_czar', 'is_host', 'is_guest', 'user']
+
+    def __init__(self, data):
+        self.user = data.get("user")
+        self.is_czar = False
+        self.is_host = data.get("host")
+        self.is_guest = bool(self.user)
+        self.name = data.get("name")
+        self.display_name = data.get("display", self.name)
+        self.id = data.get("id")
+        self.points = 0
+        self.deck = []
+
+
+class User:
+    __slots__ = ['name', 'id', 'games', 'total_points', 'total_wins']
+
+    def __init__(self, data):
+        self.name = data.get("name")
+        self.id = data.get("id")
+        self.games = []
+        self.total_points = data.get("total_points", 0)
+        self.total_wins = data.get("total_wins", 0)
+
+
+class Deck:
+    __slots__ = ['name', 'id', 'white', 'black', 'empty']
+
+    def __init__(self, data):
+        self.name = data.get("name")
+        self.id = data.get("id")
+        self.white = data.get("white", [])
+        self.black = data.get("black", [])
+        self.empty = data.get("empty", 0)
+
 
 app = Quart(__name__)
 
@@ -16,5 +67,19 @@ async def _index():
 async def _game():
     return await render_template("game.html")
 
+
+# / - Index page
+# /game - Page when in a game
+# /games/<> - View game history
+# /profile - View account profile
+# /leaderboards - View leaderboards
+# /ws - Connect to game backend
+
+# /api/profile - Returns profile information
+# /api/game/<> - Returns previous game information
+# /api/leaderboards - Returns leaderboard information
+# /api/discovery - Lists active games
+# /api/login - Endpoint for logging in
+# /api/register - Endpoint for registering
 
 app.run(host="0.0.0.0", port=80, debug=True)
